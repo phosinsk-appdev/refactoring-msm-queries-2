96f3a3060a3f0efc0049b5a31f0598bd84cfd9c0
Patrick Hosinski <phosinsk@chicagobooth.edu>
2023-05-04 15:21:28 +0000

    refactoring complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,18 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
+  
+  belongs_to(:movie, {:foreign_key => "movie_id", :class_name => "Movie"})
 
-    matching_set = Movie.where({ :id => key })
+  belongs_to(:actor, {:foreign_key => "actor_id", :class_name => "Actor"})
+  
+  # def movie
+  #   key = self.movie_id
 
-    the_one = matching_set.at(0)
+  #   matching_set = Movie.where({ :id => key })
 
-    return the_one
-  end
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
